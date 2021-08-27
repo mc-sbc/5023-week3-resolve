33812141aa4f2d4186c0f5dd5d322ed06868d452
mc-sbc <88698437+mc-sbc@users.noreply.github.com>
2021-08-27 17:27:01 +1000

    Add changes

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    established_date = '21st March 1970'
+def home():
+    established_date = '22nd March 1971'
     return render_template('index.html', established_date = established_date)
